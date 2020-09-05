face4a4fbc3cf7f5ec08705268a494c31cabb1df
cat - lin <plummorgan@gmail.com>
2020-09-05 16:44:37 -0700

    pseudocoded planner

---
+++ assets/js/script.js
@@ -0,0 +1,27 @@
+//daily planner to create a schedule
+//variable that the plans save to
+var plans = {};
+
+
+//function that creates each plan 
+var makePlan = function () {
+    
+}
+//displays current day
+//time blocks for time 
+//block for standard business hours
+//each time block will be color coated for current time, past, and future
+// when you click on time block there is input 
+//you can save event or delete
+// you can edit event or date
+
+//save event to local storage
+var savePlans = function () {
+
+}
+
+var loadPlans = function () {
+
+}
+//when we reload the task will stlll be there
+loadPlans();
