37979bcda58665d3d9dfe05e6379d5476dba028f
cat - lin <plummorgan@gmail.com>
2020-09-05 17:21:37 -0700

    cute button made / more psuedo coding done / general html laid out

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,27 +1,36 @@
 //daily planner to create a schedule
 //variable that the plans save to
 var plans = {};
+var nineToFive
 
 
 //function that creates each plan 
 var makePlan = function () {
-    
+
 }
-//displays current day
-//time blocks for time 
+
+//in the header it will display current day
 //block for standard business hours
 //each time block will be color coated for current time, past, and future
 // when you click on time block there is input 
 //you can save event or delete
+
 // you can edit event or date
+var auditTime = function () {
+
+}
+
+var auditPlan = function () {
+
+}
 
 //save event to local storage
 var savePlans = function () {
 
 }
-
 var loadPlans = function () {
 
 }
+
 //when we reload the task will stlll be there
 loadPlans();
